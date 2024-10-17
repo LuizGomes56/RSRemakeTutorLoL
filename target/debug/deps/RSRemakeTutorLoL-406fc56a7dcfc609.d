9674c35f07034e84669199bb22c763fe2de315bb
LuizGomes56 <luizgustavodgomes@gmail.com>
2024-10-17 16:18:30 -0400

    Setup

---
+++ target/debug/deps/RSRemakeTutorLoL-406fc56a7dcfc609.d
@@ -0,0 +1,5 @@
+C:\Users\Luiz Gustavo Santana\RSRemakeTutorLoL\target\debug\deps\libRSRemakeTutorLoL-406fc56a7dcfc609.rmeta: src/main.rs
+
+C:\Users\Luiz Gustavo Santana\RSRemakeTutorLoL\target\debug\deps\RSRemakeTutorLoL-406fc56a7dcfc609.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/RSRemakeTutorLoL-9c2c569d5b8a0be3.d
@@ -0,0 +1,5 @@
+C:\Users\Luiz Gustavo Santana\RSRemakeTutorLoL\target\debug\deps\libRSRemakeTutorLoL-9c2c569d5b8a0be3.rmeta: src/main.rs
+
+C:\Users\Luiz Gustavo Santana\RSRemakeTutorLoL\target\debug\deps\RSRemakeTutorLoL-9c2c569d5b8a0be3.d: src/main.rs
+
+src/main.rs:
